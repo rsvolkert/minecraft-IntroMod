da89f460b4abe515150d244af7c5449e498a5f6a
Ryan Volkert <rvolk75@gmail.com>
2023-02-16 07:45:36 -0700

    keybinding for drink water

---
+++ src/main/java/com/rvolkert/intromod/event/ClientEvents.java
@@ -0,0 +1,31 @@
+package com.rvolkert.intromod.event;
+
+import com.rvolkert.intromod.IntroMod;
+import com.rvolkert.intromod.util.KeyBinding;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+public class ClientEvents {
+    @Mod.EventBusSubscriber(modid = IntroMod.MOD_ID, value = Dist.CLIENT)
+    public static class ClientForgeEvents {
+        @SubscribeEvent
+        public static void onKeyInput(InputEvent.Key event) {
+            if(KeyBinding.DRINKING_KEY.consumeClick()) {
+                Minecraft.getInstance().player.sendSystemMessage(Component.literal("Pressed a key!"));
+            }
+        }
+    }
+
+    @Mod.EventBusSubscriber(modid = IntroMod.MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void onKeyRegister(RegisterKeyMappingsEvent event) {
+            event.register(KeyBinding.DRINKING_KEY);
+        }
+    }
+}
